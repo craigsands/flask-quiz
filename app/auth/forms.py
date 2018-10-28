2cda2971f234d353382d39e56e9d1aef408f0b34
Craig Sands <craig.sands@lexisnexisrisk.com>
2018-10-27 23:07:14 -0400

    Add optional timed quizzes.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -21,14 +21,12 @@ class RegistrationForm(FlaskForm):
                                            EqualTo('password')])
     submit = SubmitField(_l('Register'))
 
-    @staticmethod
-    def validate_username(username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different username.'))
 
-    @staticmethod
-    def validate_email(email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different email address.'))
